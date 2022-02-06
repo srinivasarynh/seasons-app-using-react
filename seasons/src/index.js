b3c4210e77799141dc65fdb5434e90576b6158c1
srinivas a r <61593110+srinivasarynh@users.noreply.github.com>
2022-02-06 14:40:57 +0530

    Add files via upload

---
+++ seasons/src/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+
+// const App = () => {
+//     window.navigator.geolocation.getCurrentPosition(
+//         (position) => console.log(position),
+//         (err) => console.log(err) 
+//     );
+//     return <div>hi there!</div>
+// };
+
+class App extends React.Component{
+    
+    state = {lat: null, errorMessage: ''};
+
+
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+            position => 
+                this.setState({lat: position.coords.latitude})
+            ,
+            err => this.setState({errorMessage: err.message})
+        );
+    }
+
+    render() {
+        if(this.state.errorMessage && !this.state.lat){
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if(!this.state.errorMessage && this.state.lat){
+            return <SeasonDisplay lat={this.state.lat} />
+        }
+
+        return <Spinner message = 'Please accept the user location' />
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
